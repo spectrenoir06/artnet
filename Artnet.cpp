01842c8fc9ec314fc6f2d1b922eebf1e3fcc9bb9
Spectre <antoine@doussaud.org>
2018-11-23 22:25:27 +0100

    add adress conf + opti

--- Artnet.cpp
+++ Artnet.cpp
@@ -28,157 +28,193 @@ Artnet::Artnet() {}
 
 void Artnet::begin(byte mac[], byte ip[])
 {
-  #if !defined(ARDUINO_SAMD_ZERO) && !defined(ESP8266) && !defined(ESP32)
-    Ethernet.begin(mac,ip);
-  #endif
+	#if !defined(ARDUINO_SAMD_ZERO) && !defined(ESP8266) && !defined(ESP32)
+		Ethernet.begin(mac,ip);
+	#endif
 
-  Udp.begin(ART_NET_PORT);
+	Udp.begin(ART_NET_PORT);
+	setDefault();
 }
 
 void Artnet::begin()
 {
-  Udp.begin(ART_NET_PORT);
+	Udp.begin(ART_NET_PORT);
+	setDefault();
 }
 
-void Artnet::setBroadcast(byte bc[])
-{
-  //sets the broadcast address
-  broadcast = bc;
+void Artnet::setDefault() {
+	ArtPollReply.subH = 0; // net
+	ArtPollReply.sub  = 0; // sub
+
+	uint8_t swin[4]  = {0x00,0x01,0x02,0x03};
+	uint8_t swout[4] = {0x00,0x01,0x02,0x03};
+	for(uint8_t i = 0; i < 4; i++)
+	{
+		ArtPollReply.swout[i] = swout[i];
+		ArtPollReply.swin[i] = swin[i];
+	}
+
+	sprintf((char *)id, "Art-Net");
+	memcpy(ArtPollReply.id, id, sizeof(ArtPollReply.id));
+	memcpy(ArtPollReply.ip, node_ip_address, sizeof(ArtPollReply.ip));
+
+	ArtPollReply.opCode = ART_POLL_REPLY;
+	ArtPollReply.port =  ART_NET_PORT;
+
+	memset(ArtPollReply.goodinput,  0x08, 4);
+	memset(ArtPollReply.goodoutput,  0x80, 4);
+	memset(ArtPollReply.porttypes,  0xc0, 4);
+
+	uint8_t shortname [18];
+	uint8_t longname [64];
+	sprintf((char *)shortname, "ESP32 Artnet");
+	sprintf((char *)longname, "ESP32 - Art-Net");
+	memcpy(ArtPollReply.shortname, shortname, sizeof(shortname));
+	memcpy(ArtPollReply.longname, longname, sizeof(longname));
+
+	ArtPollReply.etsaman[0] = 0;
+	ArtPollReply.etsaman[1] = 0;
+	ArtPollReply.verH       = 1;
+	ArtPollReply.ver        = 0;
+	ArtPollReply.oemH       = 0;
+	ArtPollReply.oem        = 0xFF;
+	ArtPollReply.ubea       = 0;
+	ArtPollReply.status     = 0xd2;
+	ArtPollReply.swvideo    = 0;
+	ArtPollReply.swmacro    = 0;
+	ArtPollReply.swremote   = 0;
+	ArtPollReply.style      = 0;
+
+	ArtPollReply.numbportsH = 0;
+	ArtPollReply.numbports  = 4;
+	ArtPollReply.status2    = 0x08;
+
+	sprintf((char *)ArtPollReply.nodereport, "%i DMX output universes active.", ArtPollReply.numbports);
 }
 
-uint16_t Artnet::read()
-{
-  packetSize = Udp.parsePacket();
+void Artnet::setBroadcast(byte bc[]) {
+	//sets the broadcast address
+	broadcast = bc;
+}
 
-  remoteIP = Udp.remoteIP();
-  if (packetSize <= MAX_BUFFER_ARTNET && packetSize > 0)
-  {
-      Udp.read(artnetPacket, MAX_BUFFER_ARTNET);
+uint16_t Artnet::read() {
+	IPAddress local_ip;
+	packetSize = Udp.parsePacket();
 
-      // Check that packetID is "Art-Net" else ignore
-      for (byte i = 0 ; i < 8 ; i++)
-      {
-        if (artnetPacket[i] != ART_NET_ID[i])
-          return 0;
-      }
+	remoteIP = Udp.remoteIP();
+	if (packetSize <= MAX_BUFFER_ARTNET && packetSize > 0)
+	{
+		Udp.read(artnetPacket, MAX_BUFFER_ARTNET);
 
-      opcode = artnetPacket[8] | artnetPacket[9] << 8;
+		// Check that packetID is "Art-Net" else ignore
+		for (byte i = 0 ; i < 8 ; i++)
+		{
+			if (artnetPacket[i] != ART_NET_ID[i])
+			return 0;
+		}
 
-      if (opcode == ART_DMX)
-      {
-        sequence = artnetPacket[12];
-        incomingUniverse = artnetPacket[14] | artnetPacket[15] << 8;
-        dmxDataLength = artnetPacket[17] | artnetPacket[16] << 8;
+		opcode = artnetPacket[8] | artnetPacket[9] << 8;
 
-        if (artDmxCallback) (*artDmxCallback)(incomingUniverse, dmxDataLength, sequence, artnetPacket + ART_DMX_START, remoteIP);
-        return ART_DMX;
-      }
-      if (opcode == ART_POLL)
-      {
-        //fill the reply struct, and then send it to the network's broadcast address
-        Serial.print("POLL from ");
-        Serial.print(remoteIP);
-        Serial.print(" broadcast addr: ");
-        Serial.println(broadcast);
+		switch (opcode) {
+			case ART_DMX:
+				sequence = artnetPacket[12];
+				incomingUniverse = artnetPacket[14] | artnetPacket[15] << 8;
+				dmxDataLength = artnetPacket[17] | artnetPacket[16] << 8;
 
-        #if !defined(ARDUINO_SAMD_ZERO) && !defined(ESP8266) && !defined(ESP32)
-          IPAddress local_ip = Ethernet.localIP();
-        #else
-          IPAddress local_ip = WiFi.localIP();
-        #endif
-        node_ip_address[0] = local_ip[0];
-      	node_ip_address[1] = local_ip[1];
-      	node_ip_address[2] = local_ip[2];
-      	node_ip_address[3] = local_ip[3];
+				if (artDmxCallback) (*artDmxCallback)(incomingUniverse, dmxDataLength, sequence, artnetPacket + ART_DMX_START, remoteIP);
+				return ART_DMX;
+			case ART_POLL:
+				//fill the reply struct, and then send it to the network's broadcast address
+				Serial.print("POLL from ");
+				Serial.print(remoteIP);
+				Serial.print(" broadcast addr: ");
+				Serial.println(broadcast);
 
-        sprintf((char *)id, "Art-Net");
-        memcpy(ArtPollReply.id, id, sizeof(ArtPollReply.id));
-        memcpy(ArtPollReply.ip, node_ip_address, sizeof(ArtPollReply.ip));
+				#if !defined(ARDUINO_SAMD_ZERO) && !defined(ESP8266) && !defined(ESP32)
+					local_ip = Ethernet.localIP();
+				#else
+					local_ip = WiFi.localIP();
+				#endif
 
-        ArtPollReply.opCode = ART_POLL_REPLY;
-        ArtPollReply.port =  ART_NET_PORT;
+				node_ip_address[0] = local_ip[0];
+				node_ip_address[1] = local_ip[1];
+				node_ip_address[2] = local_ip[2];
+				node_ip_address[3] = local_ip[3];
 
-        memset(ArtPollReply.goodinput,  0x08, 4);
-        memset(ArtPollReply.goodoutput,  0x80, 4);
-        memset(ArtPollReply.porttypes,  0xc0, 4);
+				ArtPollReply.bindip[0] = node_ip_address[0];
+				ArtPollReply.bindip[1] = node_ip_address[1];
+				ArtPollReply.bindip[2] = node_ip_address[2];
+				ArtPollReply.bindip[3] = node_ip_address[3];
 
-        uint8_t shortname [18];
-        uint8_t longname [64];
-        sprintf((char *)shortname, "artnet arduino");
-        sprintf((char *)longname, "Art-Net -> Arduino Bridge");
-        memcpy(ArtPollReply.shortname, shortname, sizeof(shortname));
-        memcpy(ArtPollReply.longname, longname, sizeof(longname));
+				Udp.beginPacket(broadcast, ART_NET_PORT);//send the packet to the broadcast address
+					Udp.write((uint8_t *)&ArtPollReply, sizeof(ArtPollReply));
+				Udp.endPacket();
 
-        ArtPollReply.etsaman[0] = 0;
-        ArtPollReply.etsaman[1] = 0;
-        ArtPollReply.verH       = 1;
-        ArtPollReply.ver        = 0;
-        ArtPollReply.subH       = 0;
-        ArtPollReply.sub        = 0;
-        ArtPollReply.oemH       = 0;
-        ArtPollReply.oem        = 0xFF;
-        ArtPollReply.ubea       = 0;
-        ArtPollReply.status     = 0xd2;
-        ArtPollReply.swvideo    = 0;
-        ArtPollReply.swmacro    = 0;
-        ArtPollReply.swremote   = 0;
-        ArtPollReply.style      = 0;
+				return ART_POLL;
+			case ART_SYNC:
+				if (artSyncCallback) (*artSyncCallback)(remoteIP);
+				return ART_SYNC;
+			case ART_ADDR:
+				Serial.print("ADRESSE size = ");
+				Serial.print(packetSize);
+				Serial.print("\topcode = ");
+				Serial.println(opcode, HEX);
 
-        ArtPollReply.numbportsH = 0;
-        ArtPollReply.numbports  = 4;
-        ArtPollReply.status2    = 0x08;
+				if (artnetPacket[12] & 0x80)
+					ArtPollReply.subH = artnetPacket[12] & 0x7F;
 
-        ArtPollReply.bindip[0] = node_ip_address[0];
-        ArtPollReply.bindip[1] = node_ip_address[1];
-        ArtPollReply.bindip[2] = node_ip_address[2];
-        ArtPollReply.bindip[3] = node_ip_address[3];
+				if (artnetPacket[104] & 0x80)
+					ArtPollReply.sub = artnetPacket[104] & 0x0F;
 
-        uint8_t swin[4]  = {0x01,0x02,0x03,0x04};
-        uint8_t swout[4] = {0x01,0x02,0x03,0x04};
-        for(uint8_t i = 0; i < 4; i++)
-        {
-            ArtPollReply.swout[i] = swout[i];
-            ArtPollReply.swin[i] = swin[i];
-        }
-        sprintf((char *)ArtPollReply.nodereport, "%i DMX output universes active.", ArtPollReply.numbports);
-        Udp.beginPacket(broadcast, ART_NET_PORT);//send the packet to the broadcast address
-        Udp.write((uint8_t *)&ArtPollReply, sizeof(ArtPollReply));
-        Udp.endPacket();
+				uint8_t *swin  = artnetPacket + 96;
+				uint8_t *swout = artnetPacket + 100;
 
-        return ART_POLL;
-      }
-      if (opcode == ART_SYNC)
-      {
-        if (artSyncCallback) (*artSyncCallback)(remoteIP);
-        return ART_SYNC;
-      }
-  }
-  else
-  {
-    return 0;
-  }
-  return 0;
+				for(uint8_t i = 0; i < 4; i++) {
+					if (swin[i] & 0x80)
+						ArtPollReply.swin[i] = swin[i] & 0x0F;
+					if (swout[i] & 0x80)
+						ArtPollReply.swout[i] = swout[i] & 0x0F;
+				}
+
+				if (artnetPacket[14])
+					memcpy(ArtPollReply.shortname, artnetPacket + 14, 18);
+
+				if (artnetPacket[32])
+					memcpy(ArtPollReply.longname, artnetPacket + 32, 64);
+
+				Udp.beginPacket(broadcast, ART_NET_PORT); //send the packet to the broadcast address
+					Udp.write((uint8_t *)&ArtPollReply, sizeof(ArtPollReply));
+				Udp.endPacket();
+
+				return ART_ADDR;
+			case default:
+				Serial.printf("opcode unknow 0x%x\n", opcode);
+				return 0
+		}
+	}
+	else
+		return 0;
 }
 
 void Artnet::printPacketHeader()
 {
-  Serial.print("packet size = ");
-  Serial.print(packetSize);
-  Serial.print("\topcode = ");
-  Serial.print(opcode, HEX);
-  Serial.print("\tuniverse number = ");
-  Serial.print(incomingUniverse);
-  Serial.print("\tdata length = ");
-  Serial.print(dmxDataLength);
-  Serial.print("\tsequence n0. = ");
-  Serial.println(sequence);
+	Serial.print("packet size = ");
+	Serial.print(packetSize);
+	Serial.print("\topcode = ");
+	Serial.print(opcode, HEX);
+	Serial.print("\tuniverse number = ");
+	Serial.print(incomingUniverse);
+	Serial.print("\tdata length = ");
+	Serial.print(dmxDataLength);
+	Serial.print("\tsequence n0. = ");
+	Serial.println(sequence);
 }
 
 void Artnet::printPacketContent()
 {
-  for (uint16_t i = ART_DMX_START ; i < dmxDataLength ; i++){
-    Serial.print(artnetPacket[i], DEC);
-    Serial.print("  ");
-  }
-  Serial.println('\n');
+	for (uint16_t i = ART_DMX_START ; i < dmxDataLength ; i++){
+		Serial.print(artnetPacket[i], DEC);
+		Serial.print("  ");
+	}
+	Serial.println('\n');
 }
--- Artnet.h
+++ Artnet.h
@@ -28,17 +28,17 @@ THE SOFTWARE.
 #include <Arduino.h>
 
 #if defined(ARDUINO_SAMD_ZERO)
-    #include <WiFi101.h>
-    #include <WiFiUdp.h>
+#include <WiFi101.h>
+#include <WiFiUdp.h>
 #elif defined(ESP8266)
-    #include <ESP8266WiFi.h>
-    #include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
 #elif defined(ESP32)
-    #include <WiFi.h>
-    #include <WiFiUdp.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
 #else
-    #include <Ethernet.h>
-    #include <EthernetUdp.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 #endif
 
 // UDP specific
@@ -48,6 +48,7 @@ THE SOFTWARE.
 #define ART_POLL_REPLY 0x2100
 #define ART_DMX 0x5000
 #define ART_SYNC 0x5200
+#define ART_ADDR 0x6000
 // Buffers
 #define MAX_BUFFER_ARTNET 530
 // Packet
@@ -55,117 +56,118 @@ THE SOFTWARE.
 #define ART_DMX_START 18
 
 struct artnet_reply_s {
-  uint8_t  id[8];
-  uint16_t opCode;
-  uint8_t  ip[4];
-  uint16_t port;
-  uint8_t  verH;
-  uint8_t  ver;
-  uint8_t  subH;
-  uint8_t  sub;
-  uint8_t  oemH;
-  uint8_t  oem;
-  uint8_t  ubea;
-  uint8_t  status;
-  uint8_t  etsaman[2];
-  uint8_t  shortname[18];
-  uint8_t  longname[64];
-  uint8_t  nodereport[64];
-  uint8_t  numbportsH;
-  uint8_t  numbports;
-  uint8_t  porttypes[4];//max of 4 ports per node
-  uint8_t  goodinput[4];
-  uint8_t  goodoutput[4];
-  uint8_t  swin[4];
-  uint8_t  swout[4];
-  uint8_t  swvideo;
-  uint8_t  swmacro;
-  uint8_t  swremote;
-  uint8_t  sp1;
-  uint8_t  sp2;
-  uint8_t  sp3;
-  uint8_t  style;
-  uint8_t  mac[6];
-  uint8_t  bindip[4];
-  uint8_t  bindindex;
-  uint8_t  status2;
-  uint8_t  filler[26];
+	uint8_t  id[8];
+	uint16_t opCode;
+	uint8_t  ip[4];
+	uint16_t port;
+	uint8_t  verH;
+	uint8_t  ver;
+	uint8_t  subH;
+	uint8_t  sub;
+	uint8_t  oemH;
+	uint8_t  oem;
+	uint8_t  ubea;
+	uint8_t  status;
+	uint8_t  etsaman[2];
+	uint8_t  shortname[18];
+	uint8_t  longname[64];
+	uint8_t  nodereport[64];
+	uint8_t  numbportsH;
+	uint8_t  numbports;
+	uint8_t  porttypes[4];//max of 4 ports per node
+	uint8_t  goodinput[4];
+	uint8_t  goodoutput[4];
+	uint8_t  swin[4];
+	uint8_t  swout[4];
+	uint8_t  swvideo;
+	uint8_t  swmacro;
+	uint8_t  swremote;
+	uint8_t  sp1;
+	uint8_t  sp2;
+	uint8_t  sp3;
+	uint8_t  style;
+	uint8_t  mac[6];
+	uint8_t  bindip[4];
+	uint8_t  bindindex;
+	uint8_t  status2;
+	uint8_t  filler[26];
 } __attribute__((packed));
 
 class Artnet
 {
 public:
-  Artnet();
+	Artnet();
 
-  void begin(byte mac[], byte ip[]);
-  void begin();
-  void setBroadcast(byte bc[]);
-  uint16_t read();
-  void printPacketHeader();
-  void printPacketContent();
+	void begin(byte mac[], byte ip[]);
+	void begin();
+	void setBroadcast(byte bc[]);
+	uint16_t read();
+	void printPacketHeader();
+	void printPacketContent();
 
-  // Return a pointer to the start of the DMX data
-  inline uint8_t* getDmxFrame(void)
-  {
-    return artnetPacket + ART_DMX_START;
-  }
+	// Return a pointer to the start of the DMX data
+	inline uint8_t* getDmxFrame(void)
+	{
+		return artnetPacket + ART_DMX_START;
+	}
 
-  inline uint16_t getOpcode(void)
-  {
-    return opcode;
-  }
+	inline uint16_t getOpcode(void)
+	{
+		return opcode;
+	}
 
-  inline uint8_t getSequence(void)
-  {
-    return sequence;
-  }
+	inline uint8_t getSequence(void)
+	{
+		return sequence;
+	}
 
-  inline uint16_t getUniverse(void)
-  {
-    return incomingUniverse;
-  }
+	inline uint16_t getUniverse(void)
+	{
+		return incomingUniverse;
+	}
 
-  inline uint16_t getLength(void)
-  {
-    return dmxDataLength;
-  }
+	inline uint16_t getLength(void)
+	{
+		return dmxDataLength;
+	}
 
-  inline IPAddress getRemoteIP(void)
-  {
-    return remoteIP;
-  }
+	inline IPAddress getRemoteIP(void)
+	{
+		return remoteIP;
+	}
 
-  inline void setArtDmxCallback(void (*fptr)(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data, IPAddress remoteIP))
-  {
-    artDmxCallback = fptr;
-  }
+	inline void setArtDmxCallback(void (*fptr)(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data, IPAddress remoteIP))
+	{
+		artDmxCallback = fptr;
+	}
 
-  inline void setArtSyncCallback(void (*fptr)(IPAddress remoteIP))
-  {
-    artSyncCallback = fptr;
-  }
+	inline void setArtSyncCallback(void (*fptr)(IPAddress remoteIP))
+	{
+		artSyncCallback = fptr;
+	}
 
 private:
-  uint8_t  node_ip_address[4];
-  uint8_t  id[8];
-  #if defined(ARDUINO_SAMD_ZERO) || defined(ESP8266) || defined(ESP32)
-    WiFiUDP Udp;
-  #else
-    EthernetUDP Udp;
-  #endif
-  struct artnet_reply_s ArtPollReply;
+	uint8_t  node_ip_address[4];
+	uint8_t  id[8];
+	#if defined(ARDUINO_SAMD_ZERO) || defined(ESP8266) || defined(ESP32)
+	WiFiUDP Udp;
+	#else
+	EthernetUDP Udp;
+	#endif
+	struct artnet_reply_s ArtPollReply;
 
 
-  uint8_t artnetPacket[MAX_BUFFER_ARTNET];
-  uint16_t packetSize;
-  IPAddress broadcast;
-  uint16_t opcode;
-  uint8_t sequence;
-  uint16_t incomingUniverse;
-  uint16_t dmxDataLength;
-  IPAddress remoteIP;
-  void (*artDmxCallback)(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data, IPAddress remoteIP);
-  void (*artSyncCallback)(IPAddress remoteIP);
+	uint8_t artnetPacket[MAX_BUFFER_ARTNET];
+	uint16_t packetSize;
+	IPAddress broadcast;
+	uint16_t opcode;
+	uint8_t sequence;
+	uint16_t incomingUniverse;
+	uint16_t dmxDataLength;
+	IPAddress remoteIP;
+	void (*artDmxCallback)(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data, IPAddress remoteIP);
+	void (*artSyncCallback)(IPAddress remoteIP);
+
 };
 
 #endif
